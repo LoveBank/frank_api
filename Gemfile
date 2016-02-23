8524b88e227b58e8372c2dea8292848f79204158
Jared L Jennings <jared@jaredjennings.org>
2016-02-23 16:14:37 -0600

    Implement Slack Notifier for registrations
    Implement a callback in the ProfileResource
    The ping only happens in production environments.

--- Gemfile
+++ Gemfile
@@ -18,6 +18,8 @@ gem 'rack-cors', :require => 'rack/cors'
 
 gem 'pg'
 
+gem 'slack-notifier'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/resources/profile_resource.rb
+++ app/resources/profile_resource.rb
@@ -2,6 +2,7 @@ class ProfileResource < JSONAPI::Resource
   model_name 'Frank::Profile'
   attributes :firstname, :lastname, :full_name, :email
   # key_type :uuid
+  after_create :slack_notify
 
   has_many :entries
   has_many :comments
@@ -21,6 +22,19 @@ class ProfileResource < JSONAPI::Resource
 
   def full_name
     "#{@model.firstname} #{@model.lastname}"
+  end
+
+
+  private
+  def slack_notify
+
+    if Rails.env.production?
+      notifier = Slack::Notifier.new ENV['SLACK_HOOK_URL_REGISTRATIONS']
+      notifier.channel = '#registrations'
+      notifier.ping "New registration #{@model.email}"
+    else
+      Rails.logger.debug "Simulating Slack ping because we are in development mode - 'New registration #{@model.email}'"
+    end
   end
 
 end

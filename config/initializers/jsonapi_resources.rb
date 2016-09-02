be114dac6efcb61b1ca7d86624b201a83a2782b6
Jared L Jennings <jared@jaredjennings.org>
2016-09-01 20:10:38 -0500

    Disable Pagination

--- config/initializers/jsonapi_resources.rb
+++ config/initializers/jsonapi_resources.rb
@@ -1,7 +1,7 @@
 JSONAPI.configure do |config|
   # built in paginators are :none, :offset, :paged
-  config.default_paginator = :offset
+  config.default_paginator = :none
 
-  config.default_page_size = 10
-  config.maximum_page_size = 50
+  #config.default_page_size = 50
+  #config.maximum_page_size = 1000
 end
